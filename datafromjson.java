8e99ca4b6cc88f4c8aed3a6c319f50d20861107f
leylagelen <leylagln534@gmail.com>
2018-12-18 11:07:46 +0300

    Edit

---
+++ datafromjson.java
@@ -0,0 +1,33 @@
+package SecondPackage;
+
+import java.io.FileReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class datafromjson {
+
+	 public static void main(String[] args) {
+	     //Create New Parser 
+		 JSONParser parser = new JSONParser();
+
+	       try {
+	    	    //Your .json file path
+	            Object obj = parser.parse(new FileReader("C:\\Users\\Leyla GELEN\\Desktop\\data1.json"));
+
+	            JSONObject jsonObject = (JSONObject) obj;
+	            //My array's name in json file 
+	            JSONArray companyList = (JSONArray) jsonObject.get("users");
+
+	            for (int i = 0; i < companyList.size(); i++) {
+	                
+	            	JSONObject objx = (JSONObject) companyList.get(i);
+
+	                System.out.println(objx.get("email") + " " + objx.get("password") + " " + objx.get("tel"));
+
+	            }
+	        }catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	 }
+}
